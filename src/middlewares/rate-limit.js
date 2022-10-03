da8f679134ad4b0d1bf4adf6d5c8d0a93c62b375
asad <asadullahkust@gmail.com>
2022-10-03 17:40:17 +0300

    update rate limiter

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -1,12 +1,14 @@
 const rateLimit = require('express-rate-limit');
 
-module.exports = () =>
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  });
+const defaultConfig = {
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+};
+
+module.exports = ({ ...override }) =>
+  rateLimit({ ...defaultConfig, ...override });
 
 // Apply the rate limiting middleware to API calls only
 // app.use('/api', apiLimiter)
